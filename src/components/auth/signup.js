6345c57bdcf3db1bf1b1a1547f55556b837081b9
Torick Davis <torickjdavis@gmail.com>
2018-07-13 19:03:13 +0000

    Built and Styled Signup Form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import SignUpForm from './signupForm';
+
 class SignUp extends Component {
 	render() {
 		return (
 			<div className="sign-up">
-        <h1>Sign Up</h1>
+        <SignUpForm />
       </div>
 		);
 	}
---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import FormTitle from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
+
+class SignUpForm extends Component {
+	render() {
+		return (
+			<form className='sign-up-form'>
+				<FormTitle text="New User"/>
+				<Field
+					className='fullName'
+					component={ FormInput }
+					name='fullName'
+					type='text'
+					placeholder='Enter Your Full Name'
+					title='Full Name'
+				/>
+				<Field
+					className='unit'
+					component={ FormInput }
+					name='unit'
+					type='text'
+					placeholder='Enter Unit #'
+					title='Unit #'
+				/>
+				<Field
+					className='email'
+					component={ FormInput }
+					name='email'
+					type='email'
+					placeholder='Enter Email'
+					title='Email'
+				/>
+				<Field
+					className='password'
+					component={ FormInput }
+					name='password'
+					type='password'
+					placeholder='Password'
+					title='Password'
+				/>
+				<Field
+					className='createAccount'
+					component={ FormButton }
+					name='createAccount'
+					type='submit'
+					title='Create Account'
+				/>
+				<div className="text-links">
+					<TextLink to='/signin' text='Already Registered? Sign In'/>
+				</div>
+			</form>
+		);
+	}
+}
+
+SignUpForm = reduxForm({
+	form: 'signup'
+})(SignUpForm);
+
+export default SignUpForm;
