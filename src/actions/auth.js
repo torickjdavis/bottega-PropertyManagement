9bb5a8242d11c9ae00bf905aef9521938eef2419
Torick Davis <torickjdavis@gmail.com>
2018-07-14 22:35:43 +0000

    AUTHENTICATE_USER reducer and related action.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 
 import { ROOT_URL } from '../config';
 
+import {
+  AUTHENTICATE_USER
+} from './types';
+
 export function signUp(fields, success) {
-  console.log(fields);
-  return function(dipatch) {
+  //console.log(fields);
+  return function(dispatch) {
     axios.post(`${ROOT_URL}/signUp`, fields) // may have to change
     .then(response => {
-      console.log(response);
+      dispatch({
+        type: AUTHENTICATE_USER,
+        payload: response.data
+      });
       success();
     })
     .catch(err => {
