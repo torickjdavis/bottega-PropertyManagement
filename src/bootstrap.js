9d5e620afb689fefca485b7514bd8516459e2b90
Torick Davis <torickjdavis@gmail.com>
2018-07-15 03:02:18 +0000

    Finished requireAuth component, and manually created browser history.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 
 import reduxThunk from 'redux-thunk';
+
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
@@ -17,18 +18,32 @@ import Layout from './components/layout';
 import SignIn from './components/auth/signin';
 import SignUp from './components/auth/signup';
 
+import requireAuth from './components/requireAuth';
+
+import history from './history';
+
+class Dashboard extends Component {
+  render() {
+    return(
+      <div>Dashboard</div>
+    );
+  }
+}
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Layout>
           <Route path='/' exact component={SignIn}/>
           <Route path='/signin' component={SignIn}/>
           <Route path='/signup' component={SignUp}/>
+          
+          <Route path='/dashboard' component={requireAuth(Dashboard)}/>
           </Layout>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </Provider>, document.querySelector('.app-wrapper'));
 }
 
