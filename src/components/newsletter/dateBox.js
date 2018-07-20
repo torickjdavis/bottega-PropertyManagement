652b1928089f6235c3dacaf80ebaca852f101f57
Torick Davis <torickjdavis@gmail.com>
2018-07-20 20:02:12 +0000

    Hooked up DateBox and NewsletterLatest to redux store to fetch data.

--- src/components/newsletter/dateBox.js
+++ src/components/newsletter/dateBox.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
 
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'];
 
-export default function DateBox({date}) {
-  return (
-    <div className="dateBox">
-      <div className="day">{ date.getDate() }</div>
-      <div className="month-year">{ monthNames[date.getMonth()]} {date.getFullYear()}</div>
-    </div>
-  );
+class DateBox extends Component {
+  render() {
+    const {date} = this.props;
+    if(!date) {
+      return <div>...Fetching Newsletters</div>;
+    }
+    
+    return (
+      <div className="dateBox">
+        <div className="day">{ date.getDate() }</div>
+        <div className="month-year">{ monthNames[date.getMonth()]} {date.getFullYear()}</div>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  const { newsletters } = state.newsletters;
+  const latestNewsletter = newsletters[0];
+  return {
+    ...latestNewsletter
+  };
+}
+
+export default connect(mapStateToProps)(DateBox);
--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -20,20 +20,12 @@ class NewsletterGrid extends Component {
   
   render() {
     
-    const latest = {
-      _id: 1337,
-      title: 'Happy Holidays',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in tellus consequat, viverra odio quis, congue nisi. Proin metus justo, feugiat at diam eu, imperdiet ullamcorper ex. Cras congue semper felis, a suscipit elit tincidunt ac. Donec pharetra luctus ligula, at fringilla tellus. Curabitur volutpat sodales elit at bibendum. Nulla ut nibh nec arcu mattis ultricies eget a risus. Vivamus vel vulputate orci, interdum imperdiet dolor. Pellentesque aliquam risus non sapien fringilla, at venenatis tortor efficitur. Fusce finibus ex mauris, non eleifend sapien sodales id. Quisque sed euismod leo. Nulla sollicitudin vestibulum urna. In consequat egestas nisl, sit amet tristique sem egestas eget. Vivamus ut neque ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent metus purus, porttitor sed est vel, vestibulum posuere diam. Curabitur pharetra eros eu ligula interdum, et porttitor arcu imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum diam lobortis arcu pellentesque, quis vehicula dui pulvinar. Praesent eu augue lacus. Phasellus non mauris at lacus maximus eleifend et eu nisl. Ut venenatis turpis ut mollis fringilla. Etiam ac arcu erat. Pellentesque tincidunt enim id eros semper tristique. Praesent sed sagittis nisi, non varius magna. Cras elit orci, convallis ac vehicula nec, vehicula eu dui. Ut vitae nulla pretium, egestas neque ut, vulputate justo. Morbi at tempor orci. Morbi scelerisque mollis mauris et venenatis. Integer lacus urna, consequat quis mauris ac, pulvinar venenatis lectus. Donec sodales mauris ut nisl lobortis, ut accumsan diam ornare. Quisque porta lectus metus, et iaculis ex ultrices vel. Donec arcu mi, rutrum quis venenatis et, ultricies vitae orci. Proin tellus metus, accumsan sit amet orci eget, lacinia sollicitudin nunc. Nunc tempus nibh quis magna luctus, at mattis felis vestibulum. Etiam rhoncus euismod nibh ut sollicitudin. Maecenas in massa elit.',
-      date: new Date(),
-      imageURL: 'http://via.placeholder.com/960x258'
-    };
-    
     return (
       <div className="newsletter-grid">
         <Button className="newsletter-grid-addButton" icon="fas fa-plus" callback={() => this.handleAddNewsletter()}/>
-        <DateBox date={new Date()} />
+        <DateBox />
         <Archive />
-        <NewsletterLatest {...latest} />
+        <NewsletterLatest />
       </div>
     );
   }
