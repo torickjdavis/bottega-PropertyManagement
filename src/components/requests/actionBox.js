32e0360979ffc4dbbf5943964043c99bdbc4b72a
Torick Davis <torickjdavis@gmail.com>
2018-07-24 22:40:35 +0000

    Added active and inactive style states for ActionBox components.

--- src/components/requests/actionBox.js
+++ src/components/requests/actionBox.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 class ActionBox extends Component {
   render() {
     const { count, title } = this.props;
+    
+    function callback() {
+      console.log('Trying to select an ActionBox');
+    }
+    
     return (
-      <div className="actionBox">
+      <a onClick={()=> callback()} className="actionBox">
         <div className="count">{count}</div>
         <div className="title">{title}</div>
-      </div>
+      </a>
     );
   }
 }
