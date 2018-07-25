8e267709e7ffde265ff665c3666bc243a108e9fb
Torick Davis <torickjdavis@gmail.com>
2018-07-25 03:29:40 +0000

    JSX for RequestItem

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Button({className, callback, text, icon}) {
+export default function Button({className, callback, icon}) {
   if(icon) {
     return (
-        <a onClick={callback} className={`${className} button`}>
+        <a onClick={callback} className={`${className ? className : ''} button`}>
           <i className={icon}></i>
         </a>
       );
--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 import Icon from '../icon';
+import Button from '../button';
 
 class RequestsItem extends Component {
   render() {
     return (
         <div className='requestsItem'>
-          <div>
-            <Icon icon='fas fa-exclamation-triangle' />
+          <Icon icon='fas fa-exclamation-triangle'/>
+          <div className='title'>
+            Hey, the thing is broken.
           </div>
+          <div className='tenant-unit'>
+            Name - Unit #
+          </div>
+          <Icon className='arrow' icon='fas fa-sort-down'/>
+          <div className='date'>
+            mm/dd/yy
+          </div>
+          <Button className='move' callback={() => console.log('Trying to change requestItem status')} icon='fas fa-wrench' />
         </div>
       );
   }
