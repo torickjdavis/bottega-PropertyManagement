336ce98c4a834f48c3bbed34023aca5d13c4b207
Torick Davis <torickjdavis@gmail.com>
2018-07-25 04:27:21 +0000

    Cleaned up RequestItem grid.

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -9,19 +9,19 @@ class RequestsItem extends Component {
         <div className='requestsItem'>
           <Icon icon='fas fa-exclamation-triangle'/>
           <div className='title'>
-            Hey, the thing is broken.
+            <p>Hey, the thing is broken.</p>
+            <Icon className='arrow' icon='fas fa-sort-down'/>
           </div>
           <div className='tenant-unit'>
             Name - Unit #
           </div>
-          <Icon className='arrow' icon='fas fa-sort-down'/>
           <div className='date'>
             mm/dd/yy
           </div>
           <Button className='move' callback={() => console.log('Trying to change requestItem status')} icon='fas fa-wrench' />
           <div className='description'>
             <img src='http://via.placeholder.com/160x94'/>
-            <p>lorem ipsum, need some text umm...</p>
+            <p>lorem ipsum, need some text umm... lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
           </div>
         </div>
       );
